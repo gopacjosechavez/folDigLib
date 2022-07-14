f63b7d1038ce1ab3b1e6b59489fc665d63a57c3e
gopacjosechavez <jose.chavez@gopac.mx>
2022-07-14 12:17:30 -0500

    liberacion inicial de la librerria para foliosdigitales
    liberacion inicial de la librerria para foliosdigitales

---
+++ Timbrar.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Xml;
+
+
+namespace folDigLib
+{
+    public class Timbrar
+    {
+        public clsDatosDeRespuestaDelTimbradoFD Documento(clsDatosParaElTimbrarFD datos)
+        {
+            clsDatosDeRespuestaDelTimbradoFD respuesta = new clsDatosDeRespuestaDelTimbradoFD();
+            
+            switch (datos.tipoTimbrado)
+            {
+                case "Pruebas":
+                    respuesta = timbrarPruebas(datos);
+                    break;
+                case "Produccion":
+                    respuesta = timbrarProductivo(datos);
+                    break;
+            }
+            return respuesta;
+        }
+
+        //------------------------------------------------------------------------
+        //  Proceso para timbrar pruebas
+        //------------------------------------------------------------------------
+        public clsDatosDeRespuestaDelTimbradoFD timbrarPruebas(clsDatosParaElTimbrarFD datos)
+        {
+            clsDatosDeRespuestaDelTimbradoFD _respuesta = new clsDatosDeRespuestaDelTimbradoFD();
+
+            //Datos para los Archivos de salida
+            string xmlSalida = datos.xmlSalida;// datos.rutaXML + datos.archivoXML;
+            string txtSalida = datos.txtSalida;// datos.rutaXML + "Historico\\" + datos.archivoTXT;
+
+            //Datos para el timbrado
+            string txtUsuario = datos.usuario;
+            string txtPassw = datos.passw;
+            string txtReferencia = datos.referencia;
+
+            
+
+            try
+            {
+                //Instancias de Pruebas
+                WSPruebas.WSCFDI33Client ws = new WSPruebas.WSCFDI33Client();
+                WSPruebas.RespuestaTFD33 respuesta = new WSPruebas.RespuestaTFD33();
+
+                //Carga el xml a timbrar
+                XmlDocument doc = new XmlDocument();
+                XmlDocument RespuetaXML = new XmlDocument();
+
+                doc.Load(xmlSalida);
+                string strXML = doc.OuterXml;
+
+                //Timbra el documento en Folios digitales
+                respuesta = ws.TimbrarCFDI(txtUsuario, txtPassw, strXML, txtReferencia);
+
+                if (respuesta.XMLResultado != "")
+                {
+                    RespuetaXML.LoadXml(respuesta.XMLResultado);
+                    RespuetaXML.Save(xmlSalida);
+                }
+
+                //Guarda los datos de respuesta del timbrado en un archivo fisico
+                _respuesta.archivo = datos.rutaXML + xmlSalida;
+                _respuesta.codigoConfirmacion = respuesta.CodigoConfirmacion;
+                _respuesta.codigoRespuesta = respuesta.CodigoRespuesta;
+                _respuesta.mensajeError = respuesta.MensajeError;
+                _respuesta.mensajeErrorDetallado = respuesta.MensajeErrorDetallado;
+                _respuesta.operacionExitosa = respuesta.OperacionExitosa.ToString();
+                _respuesta.xmlResultado = respuesta.XMLResultado;
+                _respuesta.pdfResultado = respuesta.PDFResultado;
+                clsTimbrar timbrar = new clsTimbrar();
+                timbrar.GuardarArchivoSalida(_respuesta, txtSalida);
+
+            }
+            catch (Exception ex)
+            {
+                //Guarda los datos de respuesta en case de haber un error
+                _respuesta.archivo = datos.rutaXML + xmlSalida;
+                _respuesta.codigoConfirmacion = "Error Intero";
+                _respuesta.codigoRespuesta = "Error";
+                _respuesta.mensajeError = "Error al procesar timbrado";
+                _respuesta.mensajeErrorDetallado = "ERROR: " + ex.Message;
+                _respuesta.operacionExitosa = "False";
+                _respuesta.xmlResultado = "";
+                _respuesta.pdfResultado = "";
+                clsTimbrar timbrar = new clsTimbrar();
+                timbrar.GuardarArchivoSalida(_respuesta, txtSalida);
+            }
+
+            return _respuesta;
+
+        }
+
+        //------------------------------------------------------------------------
+        //  Proceso para timbrar productivo
+        //------------------------------------------------------------------------
+        public clsDatosDeRespuestaDelTimbradoFD timbrarProductivo(clsDatosParaElTimbrarFD datos)
+        {
+            clsDatosDeRespuestaDelTimbradoFD _respuesta = new clsDatosDeRespuestaDelTimbradoFD();
+
+            //Datos para los Archivos de salida
+            string xmlSalida = datos.rutaXML + datos.archivoXML;
+            string txtSalida = datos.rutaXML + "Historico\\" + datos.archivoTXT;
+
+            //Datos para el timbrado
+            string txtUsuario = datos.usuario;
+            string txtPassw = datos.passw;
+            string txtReferencia = datos.referencia;
+
+
+
+            try
+            {
+                //Instancias de productivo
+                WSCFDI.WSCFDI33Client ws = new WSCFDI.WSCFDI33Client();
+                WSCFDI.RespuestaTFD33 respuesta = new WSCFDI.RespuestaTFD33();
+
+                //Carga el xml a timbrar
+                XmlDocument doc = new XmlDocument();
+                XmlDocument RespuetaXML = new XmlDocument();
+
+                doc.Load(xmlSalida);
+                string strXML = doc.OuterXml;
+
+                //Timbra el documento en Folios digitales
+                respuesta = ws.TimbrarCFDI(txtUsuario, txtPassw, strXML, txtReferencia);
+
+                if (respuesta.XMLResultado != "")
+                {
+                    RespuetaXML.LoadXml(respuesta.XMLResultado);
+                    RespuetaXML.Save(xmlSalida);
+                }
+
+                //Guarda los datos de respuesta del timbrado en un archivo fisico
+                _respuesta.archivo = datos.rutaXML + xmlSalida;
+                _respuesta.codigoConfirmacion = respuesta.CodigoConfirmacion;
+                _respuesta.codigoRespuesta = respuesta.CodigoRespuesta;
+                _respuesta.mensajeError = respuesta.MensajeError;
+                _respuesta.mensajeErrorDetallado = respuesta.MensajeErrorDetallado;
+                _respuesta.operacionExitosa = respuesta.OperacionExitosa.ToString();
+                _respuesta.xmlResultado = respuesta.XMLResultado;
+                _respuesta.pdfResultado = respuesta.PDFResultado;
+                clsTimbrar timbrar = new clsTimbrar();
+                timbrar.GuardarArchivoSalida(_respuesta, txtSalida);
+
+
+
+            }
+            catch (Exception ex)
+            {
+                //Guarda los datos de respuesta en case de haber un error
+                _respuesta.archivo = datos.rutaXML + xmlSalida;
+                _respuesta.codigoConfirmacion = "Error Intero";
+                _respuesta.codigoRespuesta = "Error";
+                _respuesta.mensajeError = "Error al procesar timbrado";
+                _respuesta.mensajeErrorDetallado = "ERROR: " + ex.Message;
+                _respuesta.operacionExitosa = "False";
+                _respuesta.xmlResultado = "";
+                _respuesta.pdfResultado = "";
+                clsTimbrar timbrar = new clsTimbrar();
+                timbrar.GuardarArchivoSalida(_respuesta, txtSalida);
+            }
+
+            return _respuesta;
+
+        }
+    }
+}
---
+++ clsTimbrar.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+
+namespace folDigLib
+{
+    public class clsTimbrar
+    {
+        public void GuardarArchivoSalida(clsDatosDeRespuestaDelTimbradoFD datos, string txtSalida)
+        {
+            StreamWriter sw = new StreamWriter(txtSalida);
+            try
+            {
+                if (datos.mensajeErrorDetallado == "" && datos.mensajeError != "")
+                {
+                    datos.mensajeErrorDetallado = datos.mensajeError;
+                }
+                sw.WriteLine("Archivo: " + datos.archivo);
+                sw.WriteLine("Codigo confirmacion: " + datos.codigoConfirmacion);
+                sw.WriteLine("CodigoRespuesta: " + datos.codigoRespuesta);
+                sw.WriteLine("MensajeError: " + datos.mensajeError);
+                sw.WriteLine("MensajeErrorDetallado: " + datos.mensajeErrorDetallado);
+                sw.WriteLine("OperacionExitosa: " + datos.operacionExitosa);
+                sw.WriteLine("XMLResultado: " + datos.xmlResultado);
+                sw.WriteLine("PDFResultado: " + datos.pdfResultado);
+                sw.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+
+
+    }
+
+    public class clsDatosParaElTimbrarFD
+    {
+        public string usuario { get; set; }
+        public string passw { get; set; }
+        public string referencia { get; set; }
+        public string rutaXML { get; set; }
+        public string archivoXML { get; set; }
+        public string archivoTXT { get; set; }
+        public string tipoTimbrado { get; set; }
+        public string xmlSalida { get; set; }
+        public string txtSalida { get; set; }
+
+    }
+
+    public class clsDatosDeRespuestaDelTimbradoFD
+    {
+        public string archivo { get; set; }
+        public string codigoConfirmacion { get; set; }
+        public string codigoRespuesta { get; set; }
+        public string mensajeError { get; set; }
+        public string mensajeErrorDetallado { get; set; }
+        public string operacionExitosa { get; set; }
+        public string xmlResultado { get; set; }
+        public string pdfResultado { get; set; }
+    }
+
+}
